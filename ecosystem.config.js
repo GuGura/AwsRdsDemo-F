c2d4f3f09c2955055d50264649c6bf07c79410c2
pjy <wodus331@vanillacode.net>
2024-03-27 21:06:38 +0900

    pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    apps : [{
+        name: "solve",
+        script: "npm",
+        args: "start",
+        env: {
+            NODE_ENV: "production",
+            PORT: 1212
+        },
+        watch: false,
+        exec_mode: "fork",
+        instances: 1, // CPU 코어 수만큼 인스턴스를 실행하려면 'max'를 사용하십시오.
+        autorestart: true,
+        max_memory_restart: "1G"
+    }]
+}
